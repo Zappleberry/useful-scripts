ba96a397ff6d30b507548fd428776ead58a82efc
Zappleberry <zlnelson@ucdavis.edu>
2018-11-27 11:30:19 -0800

    Brightness Check Lower priority

--- brightness-check.sh
+++ brightness-check.sh
@@ -1,1 +1,1 @@
-notify-send "$(echo "$(xrandr --verbose | grep -i eDP-1 -A 5 | grep -i Brightness | awk -F' ' '{print $2}')")"
+notify-send "$(echo "$(xrandr --verbose | grep -i eDP-1 -A 5 | grep -i Brightness | awk -F' ' '{print $2}')")" -u Low
